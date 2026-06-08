b8e15ac6d46723b8b56fe75c3696540d56756192
d96moe <moestedt@gmail.com>
2026-06-08 15:36:44 +0200

    Add defrost sensor to WLAN and CNT (from profigriller)

--- components/panasonic_ac/climate.py
+++ components/panasonic_ac/climate.py
@@ -8,9 +8,9 @@ from esphome.const import (
 )
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, climate, sensor, select, switch, text_sensor
+from esphome.components import uart, climate, sensor, select, switch, text_sensor, binary_sensor
 
-AUTO_LOAD = ["switch", "sensor", "select", "text_sensor"]
+AUTO_LOAD = ["switch", "sensor", "select", "text_sensor", "binary_sensor"]
 DEPENDENCIES = ["uart"]
 
 panasonic_ac_ns = cg.esphome_ns.namespace("panasonic_ac")
@@ -42,6 +42,7 @@ CONF_ECONAVI_SWITCH = "econavi_switch"
 CONF_MILD_DRY_SWITCH = "mild_dry_switch"
 CONF_CURRENT_POWER_CONSUMPTION = "current_power_consumption"
 CONF_ERROR_CODE = "error_code"
+CONF_DEFROST_SENSOR = "defrost_sensor"
 CONF_WLAN = "wlan"
 CONF_CNT = "cnt"
 
@@ -81,6 +82,9 @@ SCHEMA = climate._CLIMATE_SCHEMA.extend(
         cv.Optional(CONF_NANOEX_SWITCH): SWITCH_SCHEMA,
         cv.Optional(CONF_ERROR_CODE): text_sensor.text_sensor_schema(
             icon="mdi:alert-circle-outline",
+        ),
+        cv.Optional(CONF_DEFROST_SENSOR): binary_sensor.binary_sensor_schema(
+            device_class="cold",
         ),
     }
 ).extend(uart.UART_DEVICE_SCHEMA)
@@ -158,3 +162,7 @@ async def to_code(config):
     if CONF_ERROR_CODE in config:
         ts = await text_sensor.new_text_sensor(config[CONF_ERROR_CODE])
         cg.add(var.set_error_code_text_sensor(ts))
+
+    if CONF_DEFROST_SENSOR in config:
+        sens = await binary_sensor.new_binary_sensor(config[CONF_DEFROST_SENSOR])
+        cg.add(var.set_defrost_sensor(sens))
