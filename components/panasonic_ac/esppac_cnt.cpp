0c30eb2338c01ccf175ea0ae74799d03dfd6c436
d96moe <moestedt@gmail.com>
2026-08-20 15:48:48 +0200

    Remove now-redundant determine_action() call this PR replaces
    Master's set_data() (from PR #189, "Call determine_action after
    updating settings") set this->action via the generic temperature-delta
    heuristic unconditionally at the end of set_data(), after the new
    byte-12 mapping added by this PR. Left in place, it would silently
    overwrite the correct state-byte-derived action on every packet.

--- components/panasonic_ac/esppac_cnt.cpp
+++ components/panasonic_ac/esppac_cnt.cpp
@@ -408,7 +408,6 @@ void PanasonicACCNT::set_data(bool set) {
   this->update_eco(eco);
   this->update_econavi(econavi);
   this->update_mild_dry(mildDry);
-  this->action = this->determine_action();
 }
 
 /*
